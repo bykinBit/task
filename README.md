cdd9f03b174f1af446f6a341dd35b0137f744007
byking <sunbyking@gmail.com>
2023-08-24 12:26:42 +0800

    hooks api重构项目

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# task
-
-A project for React App
-
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/views/Task.jsx
+++ src/views/Task.jsx
@@ -1,4 +1,4 @@
-import React, { Component,useState,useEffect,useRef } from "react";
+import React, { Component, useState, useEffect, useRef } from "react";
 import {
   Button,
   Form,
@@ -19,7 +19,7 @@ const formatTime = function (time) {
   const [_, month, day, hours = "00", minutes = "00"] = time.match(/\d+/g);
   return `${zero(month)}-${zero(day)} ${zero(hours)}:${zero(minutes)}`;
 };
-export default function Task(){
+export default function Task() {
   //定义表的列数据
   const columns = [
     {
@@ -57,7 +57,7 @@ export default function Task(){
             <Popconfirm
               title="您确定要删除此任务吗？"
               onConfirm={() => {
-                removeTask.bind(null,id);
+                removeTaskFn(id);
               }}
             >
               <Button type="link">删除</Button>
@@ -66,7 +66,7 @@ export default function Task(){
               <Popconfirm
                 title="您确定要设置此任务状态为完成吗？"
                 onConfirm={() => {
-                  completeTask(null,id);
+                  completeTaskFn(id);
                 }}
               >
                 <Button type="link">完成</Button>
@@ -77,171 +77,150 @@ export default function Task(){
       },
     },
   ];
+  const [formIns] = Form.useForm();
   // 定义表的数据
-  state = {
-    tableData: [],
-    showVisible: false,
-    stateIndex: 0,
-    showLoading: false,
-    showButtonLoading: false,
-  };
-  let [tableData,setTableData]=useState([]);
-  let [showVisible,setShowVisible]=useState(false);
-  let [selectedIndex,setSelectIndex]=useState(0);
-  let [showLoading,setShowLoading]=useState(false);
-  let [showButtonLoading,setShowButtonLoading]=useState(false);
-  addTask = () => {
+  let [tableData, setTableData] = useState([]);
+  let [showVisible, setShowVisible] = useState(false);
+  let [selectedIndex, setSelectIndex] = useState(0);
+  let [showLoading, setShowLoading] = useState(false);
+  let [showButtonLoading, setShowButtonLoading] = useState(false);
+  useEffect(() => {
+    queryData();
+  }, [selectedIndex]);
+  let addTaskFn = () => {
     setShowVisible(true);
   };
-  submit = async () => {
-    this.setState({
-      showButtonLoading: true,
-    });
+  let submit = async () => {
     try {
-      await this.formIns.validateFields();
-      let { task, time } = this.formIns.getFieldsValue();
+      await formIns.validateFields();
+      let { task, time } = formIns.getFieldsValue();
       time = time.format("YYYY-MM-DD HH:mm:ss");
-      this.updateTask(task, time);
-      message.success("提交成功");
+      setShowButtonLoading(true);
+      updateTask(task, time);
+      message.success("新增任务成功～");
     } catch (error) {
-      message.error("提交出错");
+      message.error("新增任务失败，请稍后重试～");
     }
+    setShowButtonLoading(false);
   };
-  close = () => {
-    this.formIns.resetFields();
-    this.setState({
-      showButtonLoading: false,
-      showVisible: false,
-    });
-  };
-  changeIndex = (index) => {
-    this.setState({
-      stateIndex: index,
-    });
-    queryData(this.state.stateIndex);
-  };
-  //显示是否加载
-  showLoadingFn = (flag) => {
-    this.setState({
-      showLoading: flag,
-    });
+  const close = () => {
+    formIns.resetFields();
+    console.log(123);
+    setShowButtonLoading(false);
+    setShowVisible(false);
   };
   //查询数据
-  queryData = async (state) => {
-    this.showLoadingFn(true);
+  const queryData = async () => {
     try {
-      const { code, list } = await getTaskList(state);
+      setShowLoading(true);
+      const { code, list } = await getTaskList(selectedIndex);
       if (+code !== 0) {
         message.error("获取数据异常，请稍后重试～");
       }
-      this.setState({
-        tableData: list,
-      });
-      this.showLoadingFn(false);
+      setTableData(list);
     } catch (error) {
       message.error(error);
-      this.showLoadingFn(false);
     }
+    setShowLoading(false);
   };
   //更新数据
-  updateTask = async (task, time) => {
+  const updateTask = async (task, time) => {
     try {
       const { code } = await addTask(task, time);
       if (+code !== 0) {
         message.error("任务添加失败，请稍后重试～");
       }
       message.success("任务添加成功～");
-      this.close();
-      this.queryData(this.state.stateIndex);
+      close();
     } catch (error) {
       message.error("更新数据失败，请稍后重试～");
     }
   };
   //删除数据
-  removeTask = async (id) => {
+  const removeTaskFn = async (id) => {
     try {
       const { code } = await removeTask(id);
       if (+code !== 0) {
         message.error("任务删除失败，请稍后重试～");
       }
       message.success("删除成功～");
-      this.queryData(this.state.stateIndex);
+      queryData();
     } catch (error) {
       message.error("数据删除失败，请稍后重试～");
     }
   };
   //完成任务
-  completeTask = async (id) => {
+  const completeTaskFn = async (id) => {
     try {
       const { code } = await completeTask(id);
       if (+code !== 0) {
         message.error("操作失败，请稍后重试～");
       }
       message.success("操作成功～");
-      this.queryData(this.state.stateIndex);
+      queryData();
     } catch (error) {
       message.error("任务操作失败，请稍后重试～");
     }
   };
   return (
-      <div className="task-box">
-        <div className="header">
-          <h2 className="title">TASK OA 任务管理系统</h2>
-          <Button type="primary" onClick={this.addTask}>
-            新增任务
-          </Button>
-        </div>
-        <div className="tag-box">
-          {["全部", "未完成", "已完成"].map((val, index) => (
-            <Tag
-              onClick={()=>{
-                setSelectIndex(index)
-              }}
-              color={stateIndex === index ? "#1677ff" : ""}
-              key={index}
-            >
-              {val}
-            </Tag>
-          ))}
-        </div>
-        <Table
-          dataSource={tableData}
-          columns={columns}
-          loading={showLoading}
-          pagination={false}
-          rowKey="id"
-        ></Table>
-        <Modal
-          open={showVisible}
-          onOk={submit}
-          onCancel={close}
-          confirmLoading={showButtonLoading}
-          title="新增任务"
-        >
-          <Form
-            layout="vertical"
-            initialValues={{ task: "", time: "" }}
-          >
-            <Form.Item
-              label="任务描述"
-              name="task"
-              rules={[
-                { required: true, message: "仍务描述不可以为空" },
-                { min: 6, message: "最少需要输入6个字符" },
-              ]}
-            >
-              <Input.TextArea style={{ resize: "none" }} />
-            </Form.Item>
-            <Form.Item
-              label="预计完成时间"
-              name="time"
-              rules={[{ required: true, message: "预计完成时间不可以为空" }]}
-            >
-              <DatePicker showTime />
-            </Form.Item>
-          </Form>
-        </Modal>
+    <div className="task-box">
+      <div className="header">
+        <h2 className="title">TASK OA 任务管理系统</h2>
+        <Button type="primary" onClick={addTaskFn}>
+          新增任务
+        </Button>
       </div>
-    );
-  }
+      <div className="tag-box">
+        {["全部", "未完成", "已完成"].map((val, index) => (
+          <Tag
+            onClick={() => {
+              setSelectIndex(index);
+            }}
+            color={selectedIndex === index ? "#1677ff" : ""}
+            key={index}
+          >
+            {val}
+          </Tag>
+        ))}
+      </div>
+      <Table
+        dataSource={tableData}
+        columns={columns}
+        loading={showLoading}
+        pagination={false}
+        rowKey="id"
+      ></Table>
+      <Modal
+        open={showVisible}
+        onOk={submit}
+        onCancel={close}
+        confirmLoading={showButtonLoading}
+        title="新增任务"
+      >
+        <Form
+          layout="vertical"
+          form={formIns}
+          initialValues={{ task: "", time: "" }}
+        >
+          <Form.Item
+            label="任务描述"
+            name="task"
+            rules={[
+              { required: true, message: "仍务描述不可以为空" },
+              { min: 6, message: "最少需要输入6个字符" },
+            ]}
+          >
+            <Input.TextArea style={{ resize: "none" }} />
+          </Form.Item>
+          <Form.Item
+            label="预计完成时间"
+            name="time"
+            rules={[{ required: true, message: "预计完成时间不可以为空" }]}
+          >
+            <DatePicker showTime />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
 }
